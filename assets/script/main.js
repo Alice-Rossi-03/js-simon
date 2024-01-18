896696abe8b06d07a34d2b78b1c9a4e7dc0c3126
Alice-Rossi-03 <alicerossi0310@gmail.com>
2024-01-18 20:44:14 +0100

    fix (css & js) /style /timer-function

--- assets/script/main.js
+++ assets/script/main.js
@@ -15,11 +15,36 @@
 
 let countdownHtml = document.getElementById("countdownHtml") // prendiamo la variabile del testo 
 
-let tomorrowDate = new Date("Jan 19, 2024 09:30:00").getTime() // variabile di domani 
-let todayDate = new Date().getTime() // variabile di oggi 
+let countdownTimer = setInterval(countDownFunction, 1000) // timer  
 
-let countDown = tomorrowDate - todayDate // distanza tra i giorni 
+function countDownFunction(){ // funzione per generare il timer   
 
-console.log(countDown) // verifica in console 
+    let tomorrowDate = new Date("Jan 19, 2024 09:30:00").getTime() // variabile di domani 
+    let todayDate = new Date().getTime() // variabile di oggi 
 
-let countdownTimer = setInterval(countDownFunction, 1000) // setting timimg 
+    let countDown = tomorrowDate - todayDate // distanza tra i giorni 
+
+    console.log(countDown) // verifica in console 
+
+    let countDownSec = Math.floor(countDown / 1000) // trasformazione da millisecondi a secondi 
+
+    let seconds = Math.floor(countDownSec % 60) // trasformiamo in secondi 
+    let minutes = Math.floor((countDownSec / 60) % 60) // trasformiamo in minuti 
+    let hours = Math.floor((countDownSec / (60 * 60)) % 24) // trasformiamo in ore  
+
+    console.log(hours, minutes, seconds) // verifica in console 
+
+    if (countDown === 0){ // se il countDown raggiunge lo 0
+        clearInterval(countdownTimer)
+        countdownHtml.innerHTML = 'Lezione!'
+    } else { // altrimenti continua a calare 
+        countDown--
+        countdownHtml.innerHTML = `${hours}-${minutes}-${seconds}` // testo all'interno dell'html 
+    }
+}
+
+
+
+
+
+
